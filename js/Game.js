33eba7f82a146f496e3d8ad794f8551aaafa6dba
Youssef Girgeis <youssefgirgeis@gmail.com>
2021-10-24 23:00:25 -0400

    create the phrase and game classes and their contructors

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,11 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+  constructor(missed, phrases, activePhrase) {
+    this.missed = missed;
+    this.phrases = phrases;
+    this.activePhrase = activePhrase;
+  }
+}
